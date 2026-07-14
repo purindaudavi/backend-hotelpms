7c6ba499e409965830ff79fea1024ab746b375ac
purindaudavi <purindada.uog04@edu.lnbti.lk>
2026-07-14 16:00:31 +0530

    init 2

---
+++ server.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const app = express();
+const port = process.env.PORT || 3500;  
+
+app.get("/", (req, res) => {
+  res.send("server is running");
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
